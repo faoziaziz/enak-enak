d5b89c06ec3a8c2d5ad96a25f26b12ffc471af50
Aziz Faozi <admin@labseni.com>
2020-05-05 15:40:24 +0700

    mantap

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
- 
+import Header from './component/header.js'
 
 class App extends React.Component {
 
     render(){
         return(<div>
-               Let Say
+            <Header />
+            
+                           Let Say
                </div>);
 
     }
