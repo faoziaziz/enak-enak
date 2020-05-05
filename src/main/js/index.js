82970223553e3e75d173f063021aa377612bb1b2
Aziz Faozi <admin@labseni.com>
2020-05-05 16:23:13 +0700

    mantap

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,8 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+
 import App from './comp/App.js'
+import Jokes from './comp/Joker.js'
+import Header from './comp/Header.js'
 
 
-
-ReactDOM.render(<App />, document.getElementById('react'))
+ReactDOM.render(<Router history={createBrowserHistory()}>
+    <Switch>
+        <Route exact path='/' render={() => <Header><App /></Header>} />
+        <Route path='/jokes' render={() => <Header><Jokes /></Header>} />
+        
+    </Switch>
+</Router>, document.getElementById('react'))
 
