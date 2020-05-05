43656ca80e7aea569f64147e56ff45f82b8030c0
Aziz Faozi <admin@labseni.com>
2020-05-05 15:42:28 +0700

    sa

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -6,9 +6,8 @@ class App extends React.Component {
 
     render(){
         return(<div>
-            <Header />
-            
-                           Let Say
+                    <Header />
+                    sLet Say
                </div>);
 
     }
