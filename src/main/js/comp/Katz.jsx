3b46a22f9758efbc82d62de143276c566466a705
Aziz Faozi <admin@labseni.com>
2020-05-20 12:22:52 +0700

    mantul

--- src/main/js/comp/Katz.jsx
+++ src/main/js/comp/Katz.jsx
@@ -9,7 +9,7 @@ class Katz extends Component {
             <h2>Katz</h2> 
             <p>Halaman ini digunakan sebagai halaman utama untuk akses ke dataset. 
                 untuk data set yang ingin anda gunakan anda bisa menggunakan link berikut
-                <a href="https://faozaiziz.herokuapp.com/cities"> https://faoziaziz.herokuapp.com/cities </a>
+                <a href="https://faoziaziz.herokuapp.com/cities"> https://faoziaziz.herokuapp.com/cities </a>
                 untuk jupyter notebooknya bisa kamu dapatkan disini 
                 <a href="https://gitlab.com/azizfaozi/data-kucing/-/blob/master/notebook/AnalisaKucing.ipynb"> https://gitlab.com/azizfaozi/data-kucing/-/blob/master/notebook/AnalisaKucing.ipynb</a>
                 </p>  
